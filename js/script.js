4875c22afac6c98fec1723a4d5c2b03f38a6b5e7
Arun <jangirarun786@gmail.com>
2023-01-14 10:26:53 +0300

    Added location support

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
-/**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
- */
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
 /**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
  * Listen here: https://youtu.be/X0MDALpV29s
- *
  */
 
 $(document).on('click', function(){
@@ -87,12 +73,3 @@ var styles2 = [
     , 'font-weight: bold'
     , 'font-size: 32px'
 ].join(';');
-
-// console.log('\n\n%c SAVE THE DATE: 8th June, 2022!', styles);
-
-// console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-// console.log(
-//     `%cShaadi me zaroor aana!\n\n`,
-//     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-// )
